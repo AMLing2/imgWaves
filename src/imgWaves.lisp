f7ae2f89b39e75c470b06f6a6903d8c36bd15efb
AMLing2 <mathias.lervik@hotmail.com>
2024-12-27 00:27:45 +0100

    linear gain to pixel line added

--- src/imgWaves.lisp
+++ src/imgWaves.lisp
@@ -72,7 +72,8 @@
                             (< a (- (/ (* pi 3) 2))))) ;;wont work outside of [-2*pi to 2*pi]
 
 (defun linepoints (cx cy a imgsize) ;;no error thrown on (tan 90), so use htis then check if OOB
-  (let* ((xval) (y (if (on-right-side? a)
+  (let* ((xval)
+         (y (if (on-right-side? a)
                (prog1 ;prog1 returns first and runs both expr
                    (line-y cx cy a (first imgsize)) ;right
                  (setq xval (first imgsize)))
@@ -89,7 +90,8 @@
        (list (line-x cx cy a 0.0) 0.0))))) ;top
 
 (defun linepoints-r (cx cy a imgsize) ;;a rounded version of linepoints, mavbe remove prev
-  (let* ((xval) (y (if (on-right-side? a)
+  (let* ((xval)
+         (y (if (on-right-side? a) ;WARN: use this or prev?
                (prog1 ;prog1 returns first and runs both expr
                    (line-y cx cy a (first imgsize)) ;right
                  (setq xval (first imgsize)))
@@ -105,7 +107,7 @@
       ((< y 0.0) 
        (list (round (line-x cx cy a 0.0)) 0))))) ;top
 
-(defun linepoints-test (ang-add) ;;TODO: remove 
+(defun linepoints-test (ang-add) ;;FIX: remove 
   (let ((ang 0))
   (loop while (and (<= ang (* 2 pi)) (>= ang (- (* 2 pi))))
         do (prog1
@@ -128,9 +130,13 @@
            (expt (- (second (second p))
                     (second (first p))) 2))))
 
-(defun gen-start-points (line-num a offset imgsize)  ;;TODO: add offset
+(defun mapAdd (num apply-list)
+  (map 'list (lambda (x) (+ x num)) apply-list))
+
+(defun gen-start-points (line-num a offset imgsize)  ;;TODO: add offset, use for main func
   (let* ((ang (+ a (/ pi 2)))
-         (p (start-end-points ang imgsize))
+         (imgsizefixed (mapAdd -1 imgsize))
+         (p (start-end-points ang imgsizefixed)) ;FIX: add -1 here or something to imgsize
          (dist-x (/ (- (first (second p))
                    (first (first p))) (+ line-num 1)))
          (dist-y (/ (- (second (second p))
@@ -141,87 +147,103 @@
                          (+ (* dist-y l)
                             (second (first p)))
                          a
-                         imgsize))))
+                         imgsizefixed))))
 
-;; Image processing:
-
+;;;;; Image processing - adding gain:
+(gainline-test 5 gray-image 45 1) ;PERF: test here
 (defun add-gain (g up? index line) ;; destructve modifiicaation of line list
-  (let ((ind-move (if up? 1 -1))
+  (let ((ind-move (if up? 1 -1)) ;finished?
         (cnt 0))
-    (loop for g-add from 1 to 0 by (/ 1 g)
-          do (progn (setq cnt (+ cnt ind-move))
-              (if (or (> (+ index cnt) (list-length line))
+    (loop for g-add downfrom 255 to 0 by g ;FIX: replace 255 -> (nth index line)?
+          do (progn 
+               (setq cnt (+ cnt ind-move))
+              (if (or (> (+ index cnt) (list-length line));length -1 ?
                      (< (+ index cnt) 0))
-                 (break)
-                 (if (> (nth (+ index cnt) line) g-add)
-                     (break)
+                 (return-from add-gain nil) ;use instead of (break)
+                 (if (> (nth (+ index cnt) line) g-add); can be moved to first or?
+                     (return-from add-gain nil)
                      (setf (nth (+ index cnt) line) g-add)))))))
 
-(defun apply-gain-list (g up? index-list line)
+(defun apply-gain-list (g index-list grayscale-line)
   (dolist (i index-list)
-    (add-gain g up? i line)))
+    (add-gain g (second i) (first i) grayscale-line)))
 
-(defun find-color-jumps (grayscale-line) ;TODO:continue, err test it now i guess
+(defun find-color-jumps (grayscale-line)
   (let ((prev (first grayscale-line))
         (current)
         (index-list nil))
     (loop for i from 1 to (- (length grayscale-line) 1) by 1
           do (prog1 (setq current (nth i grayscale-line))
-               (cond ((= prev current)
-                    (setq prev (nth i grayscale-line))) ; goes up
-                     ((> (- prev current) 0)
-                      (push '(i 1) index-list)) ;adds item to front but does not matter in this context
-                     (T
-                      (push '(i 0) index-list))) ; goes down
-               (setq prev current)))))
+               (cond ((> (- prev current) 0); goes up
+                      (push (list i T) index-list)) ;adds item to front but does not matter in this context
+                     ((< (- prev current) 0); goes down
+                      (push (list i nil) index-list))) 
+               (setq prev current)))
+    (values index-list)))
 
 ;find-color-jumps test, temp
-(defun find-color-jumps-test ()
-  (let ((test-list (make-list 200 :initial-element 0)))
-    (loop for i from 120 to 160 by 1
-          do (setq (nth i test-list) 255))
+(defun find-color-jumps-test () ;TODO: test, finished?
+  (let ((test-list (fill (make-list 200 :initial-element 0)
+                         255 :start 120 :end 160)))
+; if &KEY, this means you need to use :name to fill in the variables as they are
     (print (find-color-jumps test-list))))
+(find-color-jumps-test)
 
-(defvar cummy (make-list 200 :initial-element 0))
-; end
-
-(defun make-gain-line (g image line-point) 
+(defun make-gain-line (g image line-point) ;TODO: test
   (let ((g-list nil))
-    (imago:do-line-pixels (image color x y (first (first line-point))
-                                 (second (first line-point))
-                                 (first (second line-point))
-                                 (second (second line-point)))
-      (push color g-list)) ;should be ANDed with #x00FF?
-    (setq g-list (reverse g-list))
-    ())) ; continue here with find-color-jumps and apply-gain-list
-;; image:
+    (imago:do-line-pixels (image color x y ;if 300x300 image then 0->299
+                                 (first (first line-point))   ;x1
+                                 (second (first line-point))  ;y1
+                                 (first (second line-point))  ;x2
+                                 (second (second line-point)));y2
+      (push (logand color #x00FF) g-list)) ;should be ANDed with #x00FF?
+    (print g-list)
+    (apply-gain-list g ;FIX: test
+                     (find-color-jumps g-list) ; this line works
+                     g-list); dont reverse, dosent seem to be destructive..
+    (values (reverse g-list))))                 ;a double reverse that might work well?
+
+
+(defun gain-of-lines (g image line-points) ; create a gain for all lines
+  ())
+;;;;; image:
 
 (defun gray-pixel (image x y)
   (logand (imago:image-pixel image x y) #x00FF))
 
-(defun drawpoints (point-list image) ;is this a temporary test?
+;;image tests:
+;NOTE: all of this will be moved later
+
+(defun drawpoints (point-list image)
   (dolist (p point-list)
     (prog1 (imago:draw-circle image (first (first p)) (second (first p)) 10 imago:+red+)
       (imago:draw-circle image (first (second p)) (second (second p)) 10 imago:+blue+))))
 
+(setq myimage2 (imago:make-rgb-image 600 600 imago:+white+))
+(drawpoints (gen-start-points 15 (degtorad 45) 0 (list (imago:image-width myimage2)
+                                          (imago:image-height myimage2))) myimage2)
+(defun gainline-test (g image angle line-num) ;TODO: continue
+  (let* ((imgsize (list (imago:image-width image)
+                        (imago:image-height image)))
+         (line-points (gen-start-points line-num (degtorad angle) 0 imgsize)))
+  (print line-points)
+  (print "a")
+  (print (make-gain-line g image (first line-points)))))
 
-;;image tests:
-
-(defun import-binary-image (location) ;; not using 
+(defun import-binary-image (location) ;; not using, remove
     (defvar (imago:convert-to-binary ((imago:read-image "./test.png") 10))))
 
-(defvar gray-image (imago:invert (imago:convert-to-grayscale ;use something like this
-                     (imago:read-image "~/Documents/projects/imgWaves/src/test.png"))))
+;imago:invert not in quicklisp but in the imago git repo
+;probably easy to implement self where needed
+;(defvar gray-image (imago:invert (imago:convert-to-grayscale ;use something like this
+;                     (imago:read-image "~/Documents/projects/imgWaves/src/test.png"))))
+(defvar gray-image (imago:convert-to-grayscale ;use something like this
+                     (imago:read-image "~/Documents/projects/imgWaves/src/test.png")))
 
 (print (gray-pixel gray-image 150 150))
 
 (imago:write-png gray-image "~/Documents/projects/imgWaves/bin.png")
 (import-binary-image 1)
-
-
-(setq myimage2 (imago:make-rgb-image 600 600 imago:+white+))
-(drawpoints (gen-start-points 15 (degtorad 45) 0 (list (imago:image-width myimage2)
-                                          (imago:image-height myimage2))) myimage2)
 
 (imago:write-png myimage2 "hello.png"); should write to current path...
 
