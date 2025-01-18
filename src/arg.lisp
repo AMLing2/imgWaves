04bce17c257caef6147d6009ea72e9324be76d1b
AMLing2 <mathias.lervik@hotmail.com>
2025-01-19 00:38:58 +0100

    updated argument parser

--- src/arg.lisp
+++ src/arg.lisp
@@ -1,22 +1,39 @@
 ;argparser:
 
-(setq *posix-argv* (list *posix-argv* "-rR" "-i"))
+;(setq *posix-argv* (list "sbcl" "-rR" "5" "-a" "--long_arg" "153000.0"))
 
-(defun splitArg (inString) ;;allows options such as -Rr
-  (let ((splitString (list (subseq inString 1 2))))
-    (dotimes (n (- (length inString) 2) splitString)
-      (nconc splitString (list (subseq inString (+ n 2) (+ n 3)))))))
-    ;;;(values splitString)))
+(defun first-is-dash? (arg); dont need to check if string type, *posix-argv* uses strings only
+  (equalp (char arg 0)
+          (char "-" 0)))
+(defun get-arg-val (arg-list)
+  (unless (<= (list-length arg-list) 1)
+    (unless (first-is-dash? (second arg-list))
+      (second arg-list))))
 
-(defun parseArgs () ;;(print *posix-argv*)
-  (let ((arg-list *posix-argv*))
-    (dotimes (i (- (list-length *posix-argv*) 1))
-      (setq arg-list (rest arg-list)) ;;(print arg-list)
-      (if (equalp (char (first arg-list) 0) (char "-" 0))
-          (dolist (arg (splitArg (first arg-list)) arg) 
-            (cond ((string= arg "i") (print "hello i"))
-                  ((string= arg "R") (print "hello R"))
-                  )))))) ;;it veeerks
-;;TODO: would be cool to make parseArgs a macro instead, where i can simply drop in (cond ...)
-;;TODO: add --text options, should be a really easy implementation :D
-(parseArgs)
+(defun split-arg (inString) ;;allows options such as -Rr and 
+  "return list of split argument for combinations such as -ab, passes --long_arg as unsplit string"
+  (if (and (first-is-dash? inString)
+           (equalp (char inString 1)
+                   (char "-" 0)))
+      (list (remove #\- inString)) ;if --long_arg, will remove --long-arg names, change?
+      (loop for char across (remove #\- inString)
+            collect (string char))))
+
+(defun parse-args (arg-func arg-list)
+  "iterate argv over a function ARG-FUNC with arguments (DASH-ARG VALUE), VALUE is NIL if no input and a string of the value otherwise, DASH-ARG is passed without the - , '-o' becomes 'o' and '--long_arg' becomes 'long_arg'"
+  (unless (null arg-list)
+    (when (first-is-dash? (first arg-list))
+      (dolist (arg (split-arg (first arg-list)) arg)
+        (funcall arg-func arg (get-arg-val arg-list))))
+    (parse-args arg-func (rest arg-list))))
+
+(parse-args #'arg-example *posix-argv*)
+
+(defun arg-example (dash-arg val)
+  (print (list "dash:" (string dash-arg) "val:" (string val)))
+  (cond 
+    ((string= dash-arg "a") (print "hello a"))
+    ((string= dash-arg "long_arg") (print "long arg detected"))
+    ((string= dash-arg "r") (print "split r1"))
+    ((string= dash-arg "R") (print "split R2"))))
+
